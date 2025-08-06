2e08bf043e9c8077fd3583bf66d5a36b209a511e
AlineSantos07 <aline.santos271207@gmail.com>
2025-08-06 15:25:38 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,11 +1,11 @@
 const CACHE_NAME = 'pwa-task-list-v1';
 const urlsToCache = [
-    '/Agenda/',
-    '/Agenda/index.html',
-    '/Agenda/style.css',
-    '/Agenda/manifest.json',
-    '/Agenda/icons/icon-192x192.png',
-    '/Agenda/icons/icon-512x512.png'
+    '/',
+    '/index.html',
+    '/style.css',
+    '/manifest.json',
+    '/icons/favicon-16x16.png',
+    '/icons/favicon-32x32.png'
 ];
 
 self.addEventListener('install', event => {
@@ -25,4 +25,3 @@ self.addEventListener('fetch', event => {
             })
     );
 });
-
